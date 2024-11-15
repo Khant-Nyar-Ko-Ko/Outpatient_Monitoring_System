27df57824bddf7e4bac4d56ff45357fab3baae75
khantNyarKoKo02 <6631503061@lamduan.mfu.ac.th>
2024-11-15 14:44:50 +0700

    update: logout modal

--- oms-app/src/app/components/Sidebar.tsx
+++ oms-app/src/app/components/Sidebar.tsx
@@ -8,17 +8,29 @@ import { useAuth } from "../contexts/AuthContext";
 import profile from "@/public/images/profilephoto.webp";
 import { RiFileSearchLine } from "react-icons/ri";
 import { useGetUser, useLogoutUser } from "../hooks/useAuthApi";
+import { useState } from "react"; // import useState for handling modal state
 
 const Sidebar = () => {
   const pathname = usePathname();
   const { logout } = useAuth();
   const logoutMutation = useLogoutUser();
   const { data: user } = useGetUser();
+  const [isModalOpen, setIsModalOpen] = useState(false); // state to control modal visibility
 
   const handleLogout = () => {
     logout();
     logoutMutation.mutate();
     window.location.href = "/auth/login";
+  };
+
+  // Function to show modal when logout button is clicked
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // Function to close modal
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -54,7 +66,7 @@ const Sidebar = () => {
 
       <div className="flex flex-col gap-2">
         <button
-          onClick={handleLogout}
+          onClick={showModal} // Show modal when logout is clicked
           className="flex items-center gap-4 mx-3 px-4 py-3 rounded-lg text-white"
         >
           <IoLogOutOutline size={20} />
@@ -73,6 +85,29 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-white p-5 rounded-lg shadow-lg text-center px-6 py-4">
+            <h3 className="text-lg font-semibold mb-4 text-black">Are you sure you want to log out?</h3>
+            <div className="flex justify-center gap-4 my-2">
+              <button
+                onClick={handleLogout} // Proceed with logout
+                className="bg-red-500 text-white px-4 py-2 rounded-lg"
+              >
+                Yes, Sure
+              </button>
+              <button
+                onClick={closeModal} // Cancel logout
+                className="bg-gray-500 px-4 py-2 rounded-lg text-white"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

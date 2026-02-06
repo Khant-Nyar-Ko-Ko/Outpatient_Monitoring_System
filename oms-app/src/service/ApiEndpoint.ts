9107a4414d8d92ed5d795fb468a5cf9cb51625e7
Khant Nyar Ko Ko <112542063+Khant-Nyar-Ko-Ko@users.noreply.github.com>
2026-02-06 09:57:02 +0000

    update

--- oms-app/src/service/ApiEndpoint.ts
+++ oms-app/src/service/ApiEndpoint.ts
@@ -1,1 +1,3 @@
-export const BaseURL = "https://oms-webservice-237489682555.us-central1.run.app";
+export const BaseURL =
+	(process && process.env && process.env.NEXT_PUBLIC_API_BASE_URL) ||
+	"https://outpatient-monitoring-system-1.onrender.com";

a7a29a16c139d592f09fef316b6283bee4d1f5cc
Khant Nyar Ko Ko <112542063+Khant-Nyar-Ko-Ko@users.noreply.github.com>
2026-02-06 11:17:06 +0000

    update cors access

--- oms-webservice/src/main/java/com/example/OMS/config/SecurityConfig.java
+++ oms-webservice/src/main/java/com/example/OMS/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
                 "https://oms-app-237489682555.us-central1.run.app",
-                "https://outpatient-monitoring-system-1.onrender.com"));
+                "https://outpatient-monitoring-system-1.onrender.com",
+                "https://outpatient-monitoring-system-rose.vercel.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type",
                 "Accept", "Authorization", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
